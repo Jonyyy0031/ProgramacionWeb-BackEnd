3e88cdd28734192d5f611d47004e021ecdfd5d17
Jonyyy0031 <127361357+Jonyyy0031@users.noreply.github.com>
2023-10-09 13:42:14 -0500

    Agregar archivos de proyecto.

---
+++ ProgramacionWeb-Backend/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProgramacionWeb_Backend.Models
+{
+    [Table("Personas")]
+    public class Persona
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        [StringLength(18)]
+        public string CURP { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
